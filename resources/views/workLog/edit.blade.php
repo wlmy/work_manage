62cdda7e6ffb35d444c1bed98c0907ea1c1e46d9
wulimin <wulimin@hfjy.com>
2017-09-01 20:00:31 +0800

    add code

--- resources/views/workLog/edit.blade.php
+++ resources/views/workLog/edit.blade.php
@@ -2,56 +2,122 @@
 @section('title')修改日志@endsection
 @section('scripts')
     <script type="text/javascript">
+        var value = '<?php echo $log->log_type;?>';
         $(document).ready(function () {
-            var value = '<?php echo $log->log_type;?>';
-            if(value === '0'){
+            if (value === '0') {
                 $("#daily").show();
-            }else{
+            } else {
                 $("#daily").hide();
             }
 
             if (value === '1') {
                 $("#week").show();
-            }else{
+            } else {
                 $("#week").hide();
             }
 
             if (value === '2') {
                 $("#month").show();
-            }else{
+            } else {
                 $("#month").hide();
             }
 
             if (value === '3') {
                 $("#year").show();
-            }else{
+            } else {
                 $("#year").hide();
             }
         });
 
+        function showError($msg) {
+            $("#error-message").addClass("error-message").removeClass("success-message").text($msg);
+            return false;
+        }
+
+        function showSuccess($msg) {
+            $("#error-message").addClass("success-message").removeClass("error-message").text($msg);
+            return true;
+        }
+
         $("#log-form").ajaxForm({
-            beforeSubmit : function () {
+            beforeSubmit: function () {
                 var $btn = $("button[type='submit']").button('loading');
 
-                var todayFinished = $.trim($("#todayFinished").val());
-                if(!todayFinished){
-                    $btn.button('reset');
-                    return showError("今日完成工作不能为空");
+                if (value === '0') {
+                    var todayFinished = $.trim($("#todayFinished").val());
+                    if (!todayFinished) {
+                        $btn.button('reset');
+                        return showError("今日完成工作不能为空");
+                    }
+                    var todayUnFinished = $.trim($("#todayUnFinished").val());
+                    if (!todayUnFinished) {
+                        $btn.button('reset');
+                        return showError('未完成工作不能为空');
+                    }
                 }
-                var key = $.trim($("#key").val());
-                if(!key){
-                    $btn.button('reset');
-                    return showError('键名不能为空');
+
+                if (value === '1') {
+                    var weekFinished = $.trim($("#weekFinished").val());
+                    if (!weekFinished) {
+                        $btn.button('reset');
+                        return showError("本周完成工作不能为空");
+                    }
+                    var weekSummary = $.trim($("#weekSummary").val());
+                    if (!weekSummary) {
+                        $btn.button('reset');
+                        return showError('本周工作总结不能为空');
+                    }
+                    var nextWeekPlan = $.trim($("#nextWeekPlan").val());
+                    if (!nextWeekPlan) {
+                        $btn.button('reset');
+                        return showError('下周计划不能为空');
+                    }
+                }
+
+                if (value === '2') {
+                    var monthFinished = $.trim($("#monthFinished").val());
+                    if (!monthFinished) {
+                        $btn.button('reset');
+                        return showError("本月工作内容不能为空");
+                    }
+                    var monthSummary = $.trim($("#monthSummary").val());
+                    if (!monthSummary) {
+                        $btn.button('reset');
+                        return showError('本月工作总结不能为空');
+                    }
+                    var nextMonthPlan = $.trim($("#nextMonthPlan").val());
+                    if (!nextMonthPlan) {
+                        $btn.button('reset');
+                        return showError('下月计划不能为空');
+                    }
+                }
+
+                if (value === '3') {
+                    var yearTarget = $.trim($("#yearTarget").val());
+                    if (!yearTarget) {
+                        $btn.button('reset');
+                        return showError("今年目标不能为空");
+                    }
+                    var yearPlan = $.trim($("#yearPlan").val());
+                    if (!yearPlan) {
+                        $btn.button('reset');
+                        return showError('今年关键计划不能为空');
+                    }
+                    var yearFinishSituation = $.trim($("#yearFinishSituation").val());
+                    if (!yearFinishSituation) {
+                        $btn.button('reset');
+                        return showError('完成情况不能为空');
+                    }
                 }
             },
-            success : function (res) {
+            success: function (res) {
                 $("button[type='submit']").button('reset');
-                /*if(res.errcode == 0){
+                if (res.errcode == 0) {
                     showSuccess("保存成功");
                     $("input[name='config_id']").val(res.data.id);
-                }else{
+                } else {
                     showError(res.message);
-                }*/
+                }
             }
         });
     </script>
@@ -62,15 +128,17 @@
             <h4>修改日志</h4>
         </div>
         <div class="box-body">
-            <form role="form" class="form-horizontal col-sm-5" method="post" action="{{route('workLog.create')}}" id="log-form">
-                <input type="hidden" class="form-control" name="log_id"  maxlength="20" placeholder="" value="{{$log->id}}">
+            <form role="form" class="form-horizontal col-sm-5" method="post" action="{{route('workLog.create')}}"
+                  id="log-form">
+                <input type="hidden" class="form-control" name="log_id" maxlength="20" placeholder=""
+                       value="{{$log->id}}">
                 <div class="form-group">
                     <label for="log">日志类型：</label>
                     <label style="color: #2aa198;font-weight:bold;">
                         @if($log->log_type == 0) 日报
-                            @elseif($log->log_type == 1) 周报
-                            @elseif($log->log_type == 2) 月报
-                            @elseif($log->log_type == 3) 年计划
+                        @elseif($log->log_type == 1) 周报
+                        @elseif($log->log_type == 2) 月报
+                        @elseif($log->log_type == 3) 年计划
                         @endif
                     </label>&nbsp;&nbsp;
 
@@ -78,15 +146,18 @@
                 <div id="daily" hidden>
                     <div class="form-group">
                         <label for="todayFinished">今日完成工作</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="todayFinished" id="todayFinished" maxlength="20" placeholder="" value="{{$log->today_finished}}">
+                        <input type="text" class="form-control" name="todayFinished" id="todayFinished" maxlength="20"
+                               placeholder="" value="{{$log->today_finished}}">
                     </div>
                     <div class="form-group">
                         <label for="todayUnFinished">未完成工作</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="todayUnFinished" id="todayUnFinished" maxlength="20" placeholder="" value="{{$log->today_unfinished}}">
+                        <input type="text" class="form-control" name="todayUnFinished" id="todayUnFinished"
+                               maxlength="20" placeholder="" value="{{$log->today_unfinished}}">
                     </div>
                     <div class="form-group">
                         <label for="todayConcerted">需协调工作</label>
-                        <input type="text" class="form-control" name="todayConcerted" id="todayConcerted" maxlength="20" placeholder="" value="{{$log->concerted}}">
+                        <input type="text" class="form-control" name="todayConcerted" id="todayConcerted" maxlength="20"
+                               placeholder="" value="{{$log->concerted}}">
                     </div>
                 </div>
 
@@ -94,19 +165,23 @@
                 <div id="week" hidden>
                     <div class="form-group">
                         <label for="weekFinished">本周完成工作</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="weekFinished" id="weekFinished" maxlength="20" placeholder="" value="{{$log->week_finished}}">
+                        <input type="text" class="form-control" name="weekFinished" id="weekFinished" maxlength="20"
+                               placeholder="" value="{{$log->week_finished}}">
                     </div>
                     <div class="form-group">
                         <label for="weekSummary">本周工作总结</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="weekSummary" id="weekSummary" maxlength="20" placeholder="" value="{{$log->week_summary}}">
+                        <input type="text" class="form-control" name="weekSummary" id="weekSummary" maxlength="20"
+                               placeholder="" value="{{$log->week_summary}}">
                     </div>
                     <div class="form-group">
                         <label for="nextWeekPlan">下周工作计划</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="nextWeekPlan" id="nextWeekPlan" maxlength="20" placeholder="" value="{{$log->next_week_plan}}">
+                        <input type="text" class="form-control" name="nextWeekPlan" id="nextWeekPlan" maxlength="20"
+                               placeholder="" value="{{$log->next_week_plan}}">
                     </div>
                     <div class="form-group">
                         <label for="weekConcerted">需协调与帮助</label>
-                        <input type="text" class="form-control" name="weekConcerted" id="weekConcerted" maxlength="20" placeholder="" value="{{$log->concerted}}">
+                        <input type="text" class="form-control" name="weekConcerted" id="weekConcerted" maxlength="20"
+                               placeholder="" value="{{$log->concerted}}">
                     </div>
                 </div>
 
@@ -114,56 +189,67 @@
                 <div id="month" hidden>
                     <div class="form-group">
                         <label for="monthFinished">本月工作内容</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="monthFinished" id="monthFinished" maxlength="20" placeholder="" value="{{$log->month_finished}}">
+                        <input type="text" class="form-control" name="monthFinished" id="monthFinished" maxlength="20"
+                               placeholder="" value="{{$log->month_finished}}">
                     </div>
                     <div class="form-group">
                         <label for="monthSummary">本月工作总结</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="monthSummary" id="monthSummary" maxlength="20" placeholder="" value="{{$log->month_summary}}">
+                        <input type="text" class="form-control" name="monthSummary" id="monthSummary" maxlength="20"
+                               placeholder="" value="{{$log->month_summary}}">
                     </div>
                     <div class="form-group">
                         <label for="nextMonthPlan">下月工作计划</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="nextMonthPlan" id="nextMonthPlan" maxlength="20" placeholder="" value="{{$log->next_month_plan}}">
+                        <input type="text" class="form-control" name="nextMonthPlan" id="nextMonthPlan" maxlength="20"
+                               placeholder="" value="{{$log->next_month_plan}}">
                     </div>
                     <div class="form-group">
                         <label for="monthConcerted">需帮助与支持</label>
-                        <input type="text" class="form-control" name="monthConcerted" id="monthConcerted" maxlength="20" placeholder="" value="{{$log->concerted}}">
+                        <input type="text" class="form-control" name="monthConcerted" id="monthConcerted" maxlength="20"
+                               placeholder="" value="{{$log->concerted}}">
                     </div>
                 </div>
 
                 <div id="year" hidden>
                     <div class="form-group">
                         <label for="yearTarget">今年目标</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="yearTarget" id="yearTarget" maxlength="20" placeholder="" value="{{$log->year_target}}">
+                        <input type="text" class="form-control" name="yearTarget" id="yearTarget" maxlength="20"
+                               placeholder="" value="{{$log->year_target}}">
                     </div>
                     <div class="form-group">
                         <label for="yearPlan">关键计划</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="yearPlan" id="yearPlan" maxlength="20" placeholder="" value="{{$log->year_plan}}">
+                        <input type="text" class="form-control" name="yearPlan" id="yearPlan" maxlength="20"
+                               placeholder="" value="{{$log->year_plan}}">
                     </div>
                     <div class="form-group">
                         <label for="yearFinishSituation">完成情况</label><strong class="text-danger">*</strong>
-                        <input type="text" class="form-control" name="yearFinishSituation" id="yearFinishSituation" maxlength="20" placeholder="" value="{{$log->year_plan_finished_situation}}">
+                        <input type="text" class="form-control" name="yearFinishSituation" id="yearFinishSituation"
+                               maxlength="20" placeholder="" value="{{$log->year_plan_finished_situation}}">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="remark">备注</label>
-                    <textarea class="form-control" rows="3" title="描述" name="remark" id="remark" maxlength="500">{{$log->remark}}</textarea>
+                    <textarea class="form-control" rows="3" title="描述" name="remark" id="remark"
+                              maxlength="500">{{$log->remark}}</textarea>
                 </div>
                 <div class="form-group">
                     <label for="img">图片</label>
-                    <span id="error-message"></span>
+                    <span></span>
                     {{--<div id="filePicker" class="btn btn-success" >选择</div>--}}
                     {{--<button type="button" id="saveImage" class="btn btn-success"  data-loading-text="上传中...">上传图片</button>--}}
                     <label class="btn btn-success" for="saveImage" style="color:#fff">上传文件</label>
-                    <input type="file" id="saveImage"  name="saveImage" accept="image/*" style="position:absolute;clip:rect(0 0 0 0);">
+                    <input type="file" id="saveImage" name="saveImage" accept="image/*"
+                           style="position:absolute;clip:rect(0 0 0 0);">
                 </div>
                 <div class="form-group">
                     <label for="img">附件</label>
-                    <span id="error-message"></span>
+                    <span></span>
                     {{--<div id="filePicker" class="btn btn-success" >选择</div>--}}
                     {{--<button type="button" id="saveImage" class="btn btn-success"  data-loading-text="上传中...">上传附件</button>--}}
                     <label class="btn btn-success" for="saveFile" style="color:#fff">上传文件</label>
-                    <input type="file" id="saveFile"  name="saveFile" accept="text/plain,application/vnd.ms-excel,application/msword" style="position:absolute;clip:rect(0 0 0 0);">
+                    <input type="file" id="saveFile" name="saveFile"
+                           accept="text/plain,application/vnd.ms-excel,application/msword"
+                           style="position:absolute;clip:rect(0 0 0 0);">
                 </div>
                 <div class="form-group">
                     <label for="sendToWho">发给谁</label><strong class="text-danger">*</strong>
